e860375e83c6404ab44e79433ba5a5922423a642
techstackmedia1 <osagienoah04@gmail.com>
2023-01-09 11:03:41 -0800

    implment navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+import {
+  MdOutlineNotifications,
+  MdOutlineSearch,
+  MdOutlineArrowDropDown,
+} from "react-icons/md";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+      <header className="navbar">
+        <div>
+          <img src={logo} alt="leadsqr logo" />
+          <span>leadsqr</span>
+          <input
+            type="search"
+            name=""
+            id="search"
+            placeholder="Search for anything"
+          />
+          <button title="Get Result" type="submit">
+            <MdOutlineSearch size="24" color="#fff" />
+          </button>
+        </div>
+        <div>
+          <Link to="/documentation">Docs</Link>
+          <MdOutlineNotifications size="24" color="#213F7D" />
+          <img
+            src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg"
+            alt="user's profile"
+          />
+          <span>Adedeji</span>
+          <MdOutlineArrowDropDown size="24" />
+        </div>
+      </header>
+  );
+};
+
+export default Navbar;
