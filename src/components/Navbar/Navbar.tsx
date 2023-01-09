23b225a8588be0e9392be3a4c0e431c1771bb3a6
techstackmedia1 <osagienoah04@gmail.com>
2023-01-09 15:47:23 -0800

    download images, uninstall mui, revisit navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,39 +1,37 @@
-import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
-import {
-  MdOutlineNotifications,
-  MdOutlineSearch,
-  MdOutlineArrowDropDown,
-} from "react-icons/md";
+import notification from "../../images/notification.png";
+import dropdown from "../../images/dropdown.png";
+import search from "../../images/search.png";
+import logo from "../../images/logo.png";
 import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-      <header className="navbar">
-        <div>
-          <img src={logo} alt="leadsqr logo" />
-          <span>leadsqr</span>
-          <input
-            type="search"
-            name=""
-            id="search"
-            placeholder="Search for anything"
-          />
-          <button title="Get Result" type="submit">
-            <MdOutlineSearch size="24" color="#fff" />
-          </button>
-        </div>
-        <div>
-          <Link to="/documentation">Docs</Link>
-          <MdOutlineNotifications size="24" color="#213F7D" />
-          <img
-            src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg"
-            alt="user's profile"
-          />
-          <span>Adedeji</span>
-          <MdOutlineArrowDropDown size="24" />
-        </div>
-      </header>
+    <header className="navbar">
+      <div>
+        <img src={logo} alt="leadsqr logo" />
+        <span>leadsqr</span>
+        <input
+          type="search"
+          name=""
+          id="search"
+          placeholder="Search for anything"
+        />
+        <button title="Get Result" type="submit">
+          <img src={search} alt="search icon" />
+        </button>
+      </div>
+      <div>
+        <Link to="/documentation">Docs</Link>
+        <img src={notification} alt="notification icon" />
+        <img
+          src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/592.jpg"
+          alt="user's profile"
+        />
+        <span>Adedeji</span>
+        <img src={dropdown} alt="dropdown icon" />
+      </div>
+    </header>
   );
 };
 
