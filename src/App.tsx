80ee8059592d9e537fba8cf5d2b5f416ba2b5201
techstackmedia1 <osagienoah04@gmail.com>
2023-01-09 07:59:57 -0800

    implement login ui and change logo text font-size

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
