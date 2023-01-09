05abf1927ca3c861d544140d13effca9b44330ff
techstackmedia1 <osagienoah04@gmail.com>
2023-01-09 11:20:12 -0800

    implment navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navbar />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
