0b27617afb3a859df75c42a9ee4d3f14b9f09976
techstackmedia1 <osagienoah04@gmail.com>
2023-01-10 15:46:50 -0800

    implement user table component

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -1,16 +1,39 @@
 import "./UsersTable.scss";
+import filterButton from "../../images/filter-button.png";
+import verticalMenu from "../../images/vertical-menu.png";
 const UsersTable = () => {
   return (
     <div className="UsersTable">
       <table>
         <thead>
           <tr>
-            <th>Organization</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Date Joined</th>
-            <th>Status</th>
+            <th>
+              <span>Organization</span>{" "}
+              <img src={filterButton} alt="filter button on table header" />
+            </th>
+            <th>
+              <span>Username</span>{" "}
+              <img src={filterButton} alt="filter button on table header" />
+            </th>
+            <th>
+              <span>Email</span>{" "}
+              <img
+                src={filterButton}
+                alt="filter button icon on table header"
+              />
+            </th>
+            <th>
+              <span>Phone Number</span>{" "}
+              <img src={filterButton} alt="filter button on table header" />
+            </th>
+            <th>
+              <span>Date Joined</span>{" "}
+              <img src={filterButton} alt="filter button on table header" />
+            </th>
+            <th>
+              <span>Status</span>{" "}
+              <img src={filterButton} alt="filter button on table header" />
+            </th>
             <th></th>
           </tr>
         </thead>
@@ -22,7 +45,23 @@ const UsersTable = () => {
             <td>08078903721</td>
             <td>May 15, 2020 10:00 AM</td>
             <td>Inactive</td>
-            <td></td>
+            <td>
+              {" "}
+              <img src={verticalMenu} alt="verival menu icon on table header" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>Lendsqr</td>
+            <td>Adedeji</td>
+            <td>adedeji@lendsqr.com</td>
+            <td>08078903721</td>
+            <td>May 15, 2020 10:00 AM</td>
+            <td>Inactive</td>
+            <td>
+              {" "}
+              <img src={verticalMenu} alt="verival menu icon on table header" />
+            </td>
           </tr>
         </tbody>
       </table>
