0cae1a7bf12f988be4369d74478663f3dc57131a
techstackmedia1 <osagienoah04@gmail.com>
2023-01-10 14:28:20 -0800

    first step in creating a user table component

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -1,5 +1,33 @@
+import "./UsersTable.scss";
 const UsersTable = () => {
-  return <div style={{ marginLeft: "calc(258px + 26px)" }}>User Table</div>;
+  return (
+    <div className="UsersTable">
+      <table>
+        <thead>
+          <tr>
+            <th>Organization</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Date Joined</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Lendsqr</td>
+            <td>Adedeji</td>
+            <td>adedeji@lendsqr.com</td>
+            <td>08078903721</td>
+            <td>May 15, 2020 10:00 AM</td>
+            <td>Inactive</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UsersTable;
