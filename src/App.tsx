837c5275f7a97aeb7a4047683e45aaebcc0c1933
techstackmedia1 <osagienoah04@gmail.com>
2023-01-16 14:42:54 -0800

    include menu bar to relevant page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import "./App.css";
 import Login from "./pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Detail from "./pages/Detail";
 import { useEffect, useState } from "react";
+import menuBar from "./images/menu-bar.svg";
 
 function App() {
+  const { slug } = useParams();
+  const slugText = `${slug?.toUpperCase()[0]}${slug?.slice(1)}`;
+  const menubarStyledComp = () => {
+    let styles: any;
+    if (slugText === "Login" || slugText === "") {
+      styles = {
+        display: "none",
+      };
+    } else {
+      styles = {
+        position: "absolute",
+        right: 15,
+        top: 20,
+        cursor: "pointer",
+      };
+    }
+    return styles;
+  };
+  const menuStyle = menubarStyledComp();
   const [users, setUsers] = useState([]);
   useEffect(() => {
     const fetchUsersAPI = async () => {
@@ -21,6 +41,14 @@ function App() {
 
   return (
     <BrowserRouter>
+      <img
+        className="menu"
+        style={menuStyle}
+        src={menuBar}
+        alt="menu bar icon"
+        width={30}
+        height={30}
+      />
       <Routes>
         <Route path="/dashboard" element={<Dashboard users={users} />} />
         <Route path="/dashboard/:slug" element={<Dashboard users={users} />} />
